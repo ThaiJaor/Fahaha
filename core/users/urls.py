4ddcd7ac7560b14a4960bc5faefb81b67f8fe5e8
Thai <boaboa2003@gmail.com>
2024-03-31 12:51:10 +0700

    restructure-backend-11

---
+++ core/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('login/', views.login_view, name='login'),
+    # path('register/', views.register_view, name='register'),
+    # path('logout/', views.logout_view, name='logout'),
+
+    # path('user_view/',views.user_view,name='user_view'),
+]
