f4c22a079d120eec6e5267be41f79ad87947694a
Thai <boaboa2003@gmail.com>
2024-04-02 13:09:16 +0700

    fix:domain cookie

--- core/core/settings.py
+++ core/core/settings.py
@@ -195,7 +195,7 @@ SIMPLE_JWT = {
     # A string like "example.com", or None for standard domain cookie.
     'AUTH_COOKIE_DOMAIN': 'localhost',
     # Whether the auth cookies should be secure (https:// only).
-    'AUTH_COOKIE_SECURE': False,
+    'AUTH_COOKIE_SECURE': True,
     # Http only cookie flag.It's not fetch by javascript.
     'AUTH_COOKIE_HTTP_ONLY': True,
     'AUTH_COOKIE_PATH': '/',        # The path of the auth cookie.
