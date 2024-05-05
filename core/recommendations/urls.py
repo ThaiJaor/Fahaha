a41551692849abb7d5b19d7991e250031ccd56fc
Nguyen Hung <doannguyenhung@gmail.com>
2024-05-05 12:42:45 +0700

    url for user's recommendation

--- core/recommendations/urls.py
+++ core/recommendations/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('recommendations-user/',
+         views.RecommendationForUserView.as_view(), name='recommendations-user'),
+]
