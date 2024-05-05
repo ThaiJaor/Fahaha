9d7d0ff5d78eaaaf677e098b49ff4d28a31031ef
Nguyen Hung <doannguyenhung@gmail.com>
2024-05-05 12:44:40 +0700

    add train, predict and get top n to Recommendation

--- core/recommendations/recommendation.py
+++ core/recommendations/recommendation.py
@@ -13,7 +13,6 @@ class Recommendation:
     def load_data(self):
         ratings = Rating.objects.all().values('user_id', 'book_id', 'rating')
         ratings_df = pd.DataFrame(ratings)
-        # print(ratings_df)
 
         # add pre-defined ratings to ratings_df
         books = Book.objects.all().values('id', 'rating')
@@ -23,7 +22,21 @@ class Recommendation:
         # add dumped user_id to books_df
         books_df['user_id'] = 0
         ratings_df = pd.concat([ratings_df, books_df])
-        # print(ratings_df)
 
         reader = Reader(rating_scale=(1, 5))
         self.data = Dataset.load_from_df(ratings_df, reader)
+        
+    def train(self):
+        trainset = self.data.build_full_trainset()
+        self.model = SVD()
+        self.model.fit(trainset)
+
+    def predict(self, user_id, book_id):
+        return self.model.predict(user_id, book_id).est
+
+    def get_top_n(self, user_id, n=10):
+        testset = self.data.build_full_trainset().build_anti_testset()
+        testset = filter(lambda x: x[0] == user_id, testset)
+        predictions = self.model.test(testset)
+        predictions = sorted(predictions, key=lambda x: x.est, reverse=True)
+        return predictions[:n] 
