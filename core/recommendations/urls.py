5303c5cd5c9a016702e0b2e248dbfab8615690cd
Nguyen Hung <doannguyenhung@gmail.com>
2024-05-05 12:43:08 +0700

    url for book's recommendation

--- core/recommendations/urls.py
+++ core/recommendations/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('recommendations-user/',
          views.RecommendationForUserView.as_view(), name='recommendations-user'),
+    path('recommendations-book/<int:pk>/',
+         views.RecommendationForBookView.as_view(), name='recommendations-book'),
 ]
