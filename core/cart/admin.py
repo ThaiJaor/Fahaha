3076449021bafbf343a3009fa775a5f8a0b57f4e
D@nTDM22122003 <hungdoan6969@gmail.com>
2024-05-05 12:03:22 +0700

    fix-bug

--- core/cart/admin.py
+++ core/cart/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Cart, CartItem
-# Register your models here.
+
+
+class CustomCartItemAdmin(admin.ModelAdmin):
+    def get_readonly_fields(self, request, obj=None):
+        return ['total_price', 'price', 'sale_price']
+
+
 admin.site.register(Cart)
 admin.site.register(CartItem)
