e8d1f750d03984aa010d0ed4eb761aded1cb9c57
ductin2904 <ndtin21@clc.fitus.edu.vn>
2024-05-07 04:55:00 +0700

    change favorite books serializer

--- core/favoritebooks/serializers.py
+++ core/favoritebooks/serializers.py
@@ -7,8 +7,25 @@ from books.serializers import PromotionSerializer, BookSerializer
 class FavoriteBookSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='favorite-books-detail', read_only=True)
+
     book = BookSerializer(read_only=True)
 
     class Meta:
         model = FavoriteBook
         fields = ['url', 'id', 'user', 'book']
+
+
+class FavoriteBookCreateSerializer(serializers.ModelSerializer):
+
+    book = serializers.PrimaryKeyRelatedField(
+        queryset=Book.objects.all(), write_only=True)
+
+    class Meta:
+        model = FavoriteBook
+        fields = ['book']
+
+    def validate_book(self, value):
+        if self.context['request'].user.favorite_books.filter(book=value).exists():
+            raise serializers.ValidationError(
+                "This book is already in your favorite list.")
+        return value
