f9b0a42e86cd99b0ad3563e2c662d176b8eba6a4
ductin2904 <ndtin21@clc.fitus.edu.vn>
2024-05-07 00:31:38 +0700

    adjust Categories model

--- core/books/admin.py
+++ core/books/admin.py
@@ -66,6 +66,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['created_at']
     search_fields = ['name']
     ordering = ['created_at']
+    readonly_fields = ['created_at', 'updated_at']
 
 
 admin.site.register(Book, BookAdmin)
